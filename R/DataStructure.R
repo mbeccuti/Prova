d3854dfd613e4d5b20fc79a58e538454dfb8cf04
JessicaGiordano <jessica.giordano992@gmail.com>
2017-12-05 09:57:40 +0100

    Update to 2017/12/04 discussion

--- R/DataStructure.R
+++ R/DataStructure.R
@@ -4,7 +4,7 @@
 #'
 #' @param file1 .xls file name with volume and time values
 #' @param file2 .txt file name with curve labels
-#' @return A list with 3 arguments: a matrix with 3 columns (ID curves, volume and times value), curves lengths and the grid of overall observation times.
+#' @return A list with 3 arguments: a data frame with 3 variables (ID curves, volume and times value), a vector for curves lengths and a vector for overall time grid.
 #' @examples
 #' @import readxl
 #' @export 
@@ -67,51 +67,9 @@ DataStructure <- function(file1,file2) {
   # Read Target File
   labcurv  <- read.csv(file=file2,header=TRUE)
   
-    # Ask use to feature for investigation
-  featUserChoice <- FeatureType()
-  if (featUserChoice!="none") feature <- labcurv[,featUserChoice] else feature<- NULL
-
-  # # Inizialize variables for plot
-  # labgroup    <- unique(feature)
-  # maxvol      <- max(vol)
-  # maxtimes    <- max(times)
-  nfeature    <- length(unique(feature))
-  if(!is.null(feature)) colour <- palette(rainbow(nfeature)) else colour <- "black"
-
-  # # PLOT GROWTH CURVES
-# tit         <- c(" GrowthCurve ")
-# file.name   <- paste(tit,".pdf")
-
-# #pdf(file =file.name, paper = "a4r", width = 11)
-
-   # # plot 1^st curve
-   # windows()
-   # x <- times[ID==1]
-   # y <- vol[ID==1]
-   # cl <- ifelse(!is.null(feature),colour[feature[1]],colour[1])
-   # plot(x,,col=cl,type="l",xlab='Times',ylab='Volume',cex=1.5, xlim=c(0,maxtimes), ylim=c(0,maxvol),lwd=2)
-   # points(x,y, col=cl,lwd=2,pch=16)
-   # # plot remaining curves
-   # for (i in 2:samplesize) {
-   # x <- times[ID==i]
-   # y <- vol[ID==i]
-   # cl <- ifelse(!is.null(feature),colour[feature[i]],colour[1])
-   # lines(x,y, col=cl,cex=1.5,lwd=2)
-   # points(x,y,col=cl,lwd=2,pch=16)
-   # }
-
-# # make legend and title
-# #legend("topleft", labgroup, lty=1,col=colour, title="Legend", bty="n", cex=1,lwd=2)
-# title(tit)
-# Sys.sleep(3)
-# dev.copy2pdf(device = postscript, file = "C:/Users/Jessica/Desktop/GrowthCurve.pdf",paper="a4r",width=11)
-# dev.off()
-
-   # ID, volume and time matrix
-  dataset <- data.frame(Groups=rep(feature,lencurv),ID=ID,Time=times,Vol=vol)
- # mdata    <- cbind(ID,vol,times)
-  # colnames(mdata) <- c("ID","Vol","Time")
-  alldata <- list(mydata=dataset,LenCurv=lencurv,TimeGrid=timegrid,PalColour=colour)
+  # ID, volume and time data frame
+  dataset <- data.frame(ID=ID,Time=times,Vol=vol)
+  alldata <- list(Dataset=dataset,LenCurv=lencurv,TimeGrid=timegrid)
 
   return(alldata)
 }
